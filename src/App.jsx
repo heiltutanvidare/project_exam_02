6b85f429648fcc6f0333893c219750fe1d410953
heiltutanvidare <vusvol@icloud.com>
2021-08-21 11:38:15 +0200

    Add router and temporary routing from sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,52 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./components/homepage/About/About";
 import Hero from "./components/homepage/Hero/Hero";
 import SearchPage from "./components/search/SearchPage/SearchPage";
 import Footer from "./components/ui/Footer/Footer";
 import Header from "./components/ui/Header/Header";
+import Container from "./components/layout/Container/Container";
 import { SearchProvider } from "./global/contexts/SearchContext";
 
 function App() {
 	return (
-		<main>
-			<Header />
-			<SearchProvider>
-				<Hero />
-				<SearchPage />
-			</SearchProvider>
-			<About />
-			<Footer />
-		</main>
+		<Router>
+			<main>
+				<Header />
+				<nav style={{ marginTop: "4rem" }}>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/login">Log in</Link>
+						</li>
+						<li>
+							<Link to="/admin">Admin</Link>
+						</li>
+					</ul>
+				</nav>
+				<Switch>
+					<Route path="/admin">
+						<Container>
+							<p>This will be the admin page</p>
+						</Container>
+					</Route>
+					<Route path="/login">
+						<Container>
+							<p>This will be the login page</p>
+						</Container>
+					</Route>
+					<Route path="/">
+						<SearchProvider>
+							<Hero />
+							<SearchPage />
+						</SearchProvider>
+						<About />
+					</Route>
+				</Switch>
+				<Footer />
+			</main>
+		</Router>
 	);
 }
 
