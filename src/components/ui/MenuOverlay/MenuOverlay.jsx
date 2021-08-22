0c83c2db3071fa6d9254c9ab54ce2549338685db
heiltutanvidare <vusvol@icloud.com>
2021-08-22 09:53:41 +0200

    WIP menu overlay

--- src/components/ui/MenuOverlay/MenuOverlay.jsx
+++ src/components/ui/MenuOverlay/MenuOverlay.jsx
@@ -1,30 +1,54 @@
 import Contact from "../../homepage/About/Contact/Contact";
 import Container from "../../layout/Container/Container";
 import { StyledMenuOverlay } from "./menuOverlay.styles";
+import close from "../../../assets/graphics/Close.svg";
+import MailIcon from "../../../assets/graphics/MailIcon";
+import PhoneIcon from "../../../assets/graphics/PhoneIcon";
 
 export default function MenuOverlay() {
 	return (
 		<StyledMenuOverlay>
 			<Container>
-				<h2>Reach us directly</h2>
-				<p>How can we help?</p>
-				<address>
+				<button className="menu__close__button">
+					Close <img src={close} alt="Close the overlay meny" />
+				</button>
+				<h2 className="menu__title">
+					Reach us <span>directly</span>
+				</h2>
+				<p className="menu__text">How can we help?</p>
+				<address className="menu__address">
 					Holidaze <br />
 					220 Sleepover Blv <br />
 					5020 Bergen NO
 				</address>
-				<a href="mailto:hey@holidaze.com">hey@holidaze.com</a>
-				<a href="tel:04791607947">+47 916 07 947</a>
+				<div className="menu__links__container">
+					<a className="menu__link" href="mailto:hey@holidaze.com">
+						<div className="menu__icon__container">
+							<MailIcon />
+						</div>
+						hey@holidaze.com
+					</a>
+					<a className="menu__link" href="tel:04791607947">
+						<div className="menu__icon__container">
+							<PhoneIcon />
+						</div>
+						+47 916 07 947
+					</a>
+				</div>
 				<p>Opening hours</p>
-				<p>
+				<p className="menu__opening-hours">
 					Mon–fri <span>08:00–16:00</span>
 				</p>
-				<p>
+				<p className="menu__opening-hours">
 					Saturday <span>09:00–15:00</span>
 				</p>
 
-				<h2>Enquire about an accommodation?</h2>
-				<p>We always respond within 24 working hours</p>
+				<h2 className="menu__title">
+					Enquire about <span>an accommodation?</span>
+				</h2>
+				<p className="menu__text">
+					We always respond within 24 working hours
+				</p>
 				<Contact />
 			</Container>
 		</StyledMenuOverlay>
--- src/components/ui/MenuOverlay/menuOverlay.styles.js
+++ src/components/ui/MenuOverlay/menuOverlay.styles.js
@@ -1,13 +1,88 @@
 import styled from "styled-components";
 
 export const StyledMenuOverlay = styled.div`
+	padding-top: var(--space-xsm);
 	position: fixed;
 	top: 0;
 	width: 85%;
 	right: 0;
-	min-height: 100vh;
+	bottom: 0;
 	margin-left: auto;
+	overflow-y: scroll;
 	background-color: var(--clr-primary-900);
 	color: var(--clr-drk-50);
 	z-index: 3000;
+
+	.menu {
+		&__close__button {
+			margin-left: auto;
+			background: none;
+			color: var(--clr-drk-50);
+			display: flex;
+			align-items: center;
+			border: none;
+			outline: none;
+			border-radius: none;
+			text-transform: uppercase;
+			font-size: 0.625rem;
+			letter-spacing: 0.25em;
+
+			img {
+				border: 1px solid var(--clr-drk-50);
+				padding: 0.5rem;
+				border-radius: 50%;
+				fill: red;
+			}
+		}
+
+		&__title {
+			margin-top: var(--space-xsm);
+			font-family: Viaoda Libre;
+			font-style: normal;
+			font-weight: normal;
+			font-size: 1.375rem;
+			line-height: 1.8rem;
+
+			span {
+				display: block;
+			}
+		}
+
+		&__text {
+			font-size: 0.875rem;
+			margin: var(--space-xxsm) 0;
+		}
+
+		&__address {
+			font-style: normal;
+			font-weight: 300;
+			font-size: 0.75rem;
+		}
+
+		&__links__container {
+			margin: 2.5rem 0;
+			display: grid;
+			grid-gap: 1rem;
+		}
+
+		&__link {
+			color: inherit;
+			text-decoration: none;
+			display: flex;
+			align-items: center;
+
+			svg {
+				display: block;
+				width: 1.5rem;
+				outline: 1px solid pink;
+				padding: 0.25rem;
+				background: white;
+				border-radius: 50%;
+
+				path {
+					fill: var(--clr-drk-900);
+				}
+			}
+		}
+	}
 `;
