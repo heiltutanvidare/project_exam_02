aa3cb285ed5e0256ca2323b75aa7d645862850e3
heiltutanvidare <vusvol@icloud.com>
2021-09-21 08:45:54 +0200

    Feature: Add width prop to container component

--- src/components/layout/Container/Container.jsx
+++ src/components/layout/Container/Container.jsx
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
 import { StyledContainer } from "./container.styles";
 
-export default function Container({ children }) {
-	return <StyledContainer>{children}</StyledContainer>;
+export default function Container({ children, maxWidth }) {
+	return <StyledContainer maxWidth={maxWidth}>{children}</StyledContainer>;
 }
 
 Container.propTypes = {
 	children: PropTypes.node.isRequired,
+	maxWidth: PropTypes.string,
+};
+
+Container.defaultProps = {
+	maxWidth: "var(--max-width)",
 };
--- src/components/layout/Container/container.styles.js
+++ src/components/layout/Container/container.styles.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-	max-width: var(--max-width);
+	max-width: ${(props) =>
+		props.maxWidth ? props.maxWidth : "var(--max-width)"};
 	width: 85%;
 	margin-left: auto;
 	margin-right: auto;
